545c8a5b870afdac7270eb96557b79695af13576
(Amelie) <leopamsfairweather@Gmail.com>
2025-10-20 20:40:08 -0400

    hi

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,17 @@ import '../styles/style.css';
 import '../styles/styled.css';
 import '../styles/styler.css';
 import '../styles/styley.css';
-import { getServerSession } from "next-auth/next";
-import AuthProvider from "../components/AuthProvider";
 
 export const metadata = {
   title: 'RIDE',
   description: 'Lets get you where you need to go',
 };
 
-export default async function RootLayout({ children }) {
-  const session = await getServerSession();
-  
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AuthProvider session={session}>
-          {children}
-        </AuthProvider>
+        {children}
       </body>
     </html>
   );
